464b985d0c381b5846cbba8a95266e1824b363cc
unknown <pedrovitorcomercial@gmail.com>
2024-08-20 23:21:48 -0300

    Finished lesson function definitions

---
+++ Cap11 - Functions/FunctionsDefinitions.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+double circle_area_calculator(double radius);
+double cylinder_volume_calculator(double radius, double eigth);
+void get_radio_value();
+void get_height_value(double radius) ;
