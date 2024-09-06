04288443e8f21bdc9b01be494ec47a1ce2177c3a
unknown <pedrovitorcomercial@gmail.com>
2024-09-05 22:31:00 -0300

    Finished lesson Exercise - Coverting Temperatures

---
+++ Cap11 - Functions/ExerciseConvertingTemperatures.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+#include <chrono>
+#include <thread>
+#include <windows.h>
+
+using namespace std;
+
+double convert_celcius_Fahrenheit(double);
+double convert_celcius_Kelvin(double);
+void show_loading();
