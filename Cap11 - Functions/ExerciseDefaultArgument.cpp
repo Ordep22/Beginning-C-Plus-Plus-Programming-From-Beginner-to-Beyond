c8034990112402b1321b7b142ca3702bbc8938fb
unknown <pedrovitorcomercial@gmail.com>
2024-09-07 20:50:36 -0300

    Finished lesson Exercise Defaut Arguments

---
+++ Cap11 - Functions/ExerciseDefaultArgument.cpp
@@ -0,0 +1,28 @@
+#include "ExerciseDefaultArgument.h"
+
+int main()
+{
+
+    cout << "For the first week" << endl;
+    PrintGroceryList();
+
+    cout << "For the second week" << endl;
+    PrintGroceryList(20,10,1);
+
+
+
+
+}
+
+
+void PrintGroceryList(int mangos, int apples, int oranges)
+{
+    cout << "======================================" << endl;
+    cout << "The gorcerry List is:" << endl;
+    cout << "Mangos: " << mangos << endl;
+    cout << "Apples: " << apples << endl;
+    cout << "Oranges: " << oranges << endl;
+    cout << "======================================" << endl;
+
+ 
+}
---
+++ Cap11 - Functions/ExerciseDefaultArgument.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintGroceryList(int mangos = 13, int apples = 3, int oranges = 7 );
