34c6be23b4fec34dab2275b90f83a558a9b23640
unknown <pedrovitorcomercial@gmail.com>
2024-09-24 23:18:14 -0300

    Finished lesson: Programming Exercise 29 - Implementing a Recursive Function

---
+++ Cap11 - Functions/RecursionExerciseTwo.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int actualNumberofDays {0};
+double amount {0};
+double penny {0.01};
+
+int function_activation_count(int);
+
+int function_activation_count(int numberOfDays)
+{
+
+    actualNumberofDays = numberOfDays;
+    
+    actualNumberofDays -= 1;
+
+    amount = penny*pow(2,actualNumberofDays);
+
+    cout << amount << endl;
+
+    if (actualNumberofDays <= 0)
+    
+        return amount;
+
+    return function_activation_count(actualNumberofDays);
+
+
+
+
+
+
+}
+
+
+int main()
+{
+
+    function_activation_count(16);
+
+
+}
